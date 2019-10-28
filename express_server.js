087f0b139f293f51a5b196a86bf8b72541322064
dbeattie <darren@fettle.ca>
2019-10-28 19:28:49 +0000

    added a header partial and updated the other files to display that header

--- express_server.js
+++ express_server.js
@@ -15,7 +15,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.shortURL };
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
 
