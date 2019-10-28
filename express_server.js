1e3d664662a8b78d95d373b7e1bd50267e84a648
dbeattie <darren@fettle.ca>
2019-10-28 18:07:45 +0000

    fixed it so that /hello and root location work

--- express_server.js
+++ express_server.js
@@ -11,7 +11,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
