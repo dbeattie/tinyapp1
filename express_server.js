9d62f716115f7bf214028bc41224c2722f79d629
dbeattie <darren@fettle.ca>
2019-10-29 20:38:21 +0000

    minor tweaks and commenting out some code to make things cleaner

--- express_server.js
+++ express_server.js
@@ -23,11 +23,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//RENDERS urls_new template
+//RENDERS A NEW URLS_ TEMPLATE
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//CREATES A RANDOM 6 CHARACTER FUNCTION THAT CAN PASS INTO THE SHORTURL PARAMETER
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   
@@ -36,34 +37,35 @@ app.post("/urls", (req, res) => {
   res.redirect('/urls');
 });
 
+//ASSOCIATES ANY SHORT URL DATA WITH THAT ENTIRE KEY AND VALUE FROM MY STORED DATA
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+//
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-//REDIRECT AFTER HITTING EDIT BUTTON ON URL Main Page
+//REDIRECT AFTER HITTING EDIT BUTTON ON URL TO THAT URL SUBPAGE
 app.post("/urls/:shortURL/edit", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 })
 
-//REDIRECT AFTER DELETING A SHORT URL
+//REDIRECT AFTER DELETING AN ENTIRE URL FROM DATABASE
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 })
 
-//REDIRECT AFTER UPDATING LONG URL on urls/:shortURL
+//REDIRECT AFTER UPDATING LONG URL FROM URLS_SHOW
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body['longURL'];
   res.redirect('/urls');
 })
-
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
