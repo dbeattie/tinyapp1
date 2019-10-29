6c955ed13ba070e1a3b3f55640f18da8d5bf7691
dbeattie <darren@fettle.ca>
2019-10-29 23:24:10 +0000

    using cookies, create a login and logout input and button setup in the header template

--- express_server.js
+++ express_server.js
@@ -7,6 +7,10 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
+
+//GENERATES 6 RANDOM CHARACTERS
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
 }
@@ -17,15 +21,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//DO I NEED THIS???
+// app.get('', (req, res) => {
+//   let templateVars = {
+//   username: req.cookies["username"],
+//   // ... any other vars
+// };
+//   res.render("urls_index", templateVars);
+// });
+
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies.username };
   res.render("urls_index", templateVars);
 });
 
 //RENDERS A NEW URLS_ TEMPLATE
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies.username }
+  res.render("urls_new", templateVars);
 });
 
 //CREATES A RANDOM 6 CHARACTER FUNCTION THAT CAN PASS INTO THE SHORTURL PARAMETER
@@ -39,7 +53,7 @@ app.post("/urls", (req, res) => {
 
 //ASSOCIATES ANY SHORT URL DATA WITH THAT ENTIRE KEY AND VALUE FROM MY STORED DATA
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
   res.render("urls_show", templateVars);
 });
 
@@ -51,7 +65,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 //REDIRECT AFTER HITTING EDIT BUTTON ON URL TO THAT URL SUBPAGE
 app.post("/urls/:shortURL/edit", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
   res.render("urls_show", templateVars);
 })
 
@@ -66,6 +80,19 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body['longURL'];
   res.redirect('/urls');
 })
+
+//ROUTE TO LOGIN
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body['username']);
+  res.redirect('/urls');
+})
+
+//ROUTE TO LOGOUT
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
